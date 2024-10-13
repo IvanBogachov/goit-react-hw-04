9e538a9eb52975193ee07bf3a88daeb4c6cd5887
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-10-13 10:48:57 +0300

    fix error

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,15 +1,21 @@
 import styles from "./ImageCard.module.css";
 
-const ImageCard = ({ alt_description, urls, updateModalStateData }) => {
+const ImageCard = ({
+  alt_description,
+  urls,
+  updateModalStateData,
+  openModal,
+}) => {
   return (
-    <div
-      className={styles.cardWrapper}
-      onClick={() => updateModalStateData(urls.regular, alt_description)}
-    >
+    <div className={styles.cardWrapper}>
       <img
         className={styles.cardImage}
         src={urls.small}
         alt={alt_description}
+        onClick={() => {
+          updateModalStateData(urls.regular, alt_description);
+          openModal();
+        }}
       />
       <p className={styles.cardDescription}>{alt_description}</p>
     </div>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,11 +5,12 @@ const ImageGallery = ({ gallery, openModal, updateModalStateData }) => {
   return (
     <ul className={styles.itemsContainer}>
       {gallery.map(({ id, alt_description, urls }) => (
-        <li className={styles.cardItem} key={id} onClick={openModal}>
+        <li className={styles.cardItem} key={id}>
           <ImageCard
             urls={urls}
             alt_description={alt_description}
             updateModalStateData={updateModalStateData}
+            openModal={openModal}
           />
         </li>
       ))}
